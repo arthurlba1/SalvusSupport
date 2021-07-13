db1656af66f1057d2b6a0f727a920298f9cb0bf6
arthurlba1 <1arthurluiz1@gmail.com>
2021-07-13 14:19:14 -0300

    resources project

---
+++ resources/tables.sql
@@ -0,0 +1,48 @@
+--postgres 12
+
+CREATE TABLE profission 
+(
+	prof_Id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	prof_name VARCHAR NOT NULL
+);
+
+CREATE TABLE speciality
+(
+	specId int GENERATED ALWAYS AS IDENTITY,
+	spec_name VARCHAR NOT NULL,
+	prof_id_fk int NOT NULL,
+	CONSTRAINT profId_spec_fk FOREIGN KEY (prof_id_fk) REFERENCES profission(prof_Id) 
+);
+
+CREATE TABLE professional_info
+(	
+	profId int GENERATED ALWAYS AS IDENTITY,
+	prof_id_fk int NOT NULL,
+	prof_reg_number VARCHAR NOT NULL,
+	prof_number VARCHAR NOT NULL,
+	prof_location VARCHAR NOT NULL,
+	prof_displacement VARCHAR NOT NULL,
+	prof_spec int[],
+	CONSTRAINT profId_prof_fk FOREIGN KEY (prof_id_fk) REFERENCES profission(prof_Id) 
+);
+
+CREATE TABLE users 
+(
+	userId int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  
+	email VARCHAR NOT NULL,
+	full_name VARCHAR NOT NULL,
+	pass VARCHAR NOT NULL,
+	login VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	avatar BYTEA NOT NULL
+);
+
+CREATE TABLE doc_upload
+(
+	doc_id int GENERATED ALWAYS AS IDENTITY,
+	user_id_fk INT NOT NULL,
+	file_content BYTEA NOT NULL,
+	file_name VARCHAR NOT NULL,
+	CONSTRAINT user_doc FOREIGN KEY (user_id_fk) REFERENCES users(userId) 
+);
