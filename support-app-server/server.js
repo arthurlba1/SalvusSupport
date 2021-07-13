ceb6f922890c593964f1e434892c23c0d3a21f60
arthurlba1 <1arthurluiz1@gmail.com>
2021-07-13 18:15:29 -0300

    get methods and submission to database

--- support-app-server/server.js
+++ support-app-server/server.js
@@ -25,7 +25,7 @@ app.get("/",(req,res) =>{
 
 app.get("/getProf", (req,res) => {
     var pool = openConnection();
-    pool.query("SELECT prof_name, prof_id FROM profission" , (err, result) => {
+    pool.query("SELECT prof_name FROM profission" , (err, result) => {
         res.send(result.rows)
         pool.end();
     });
@@ -33,7 +33,7 @@ app.get("/getProf", (req,res) => {
 
 app.get("/getProfSpec", (req,res) => {
     var pool = openConnection();
-    pool.query("SELECT s.spec_name from profission INNER JOIN speciality s ON s.prof_id_fk = prof_id" , (err, result) => {
+    pool.query("SELECT prof_id, prof_name, s.specid, s.spec_name from profission INNER JOIN speciality s ON s.prof_id_fk = prof_id" , (err, result) => {
         res.send(result.rows)
         pool.end();
     });
@@ -42,17 +42,26 @@ app.get("/getProfSpec", (req,res) => {
 app.post("/createUser",(req,res) => {
     var data = (req.body);
     var pool = openConnection();
-    let query = "INSERT INTO users (email, full_name, pass, login) VALUES($1, $2, $3, $4)";
-    pool.query(query, [data.email, data.name, data.password, data.user] , (err, result) =>{
-        if (err) {
-            console.log(err.stack)
-          } else {
-            console.log(result.rows[0])
-          }
+    let profQuery = "INSERT INTO professional_info (prof_id_fk, prof_reg_number, prof_number, prof_location, prof_displacement, prof_spec)"
+    + "VALUES ($1, $2, $3, $4, $5, $6) RETURNING profid"
+    console.log([data.profission, data.regNumber, data.telephone, data.location, data.displacement, data.speciality]);
+    pool.query(profQuery, [data.profission, data.regNumber, data.telephone, data.location, data.displacement, data.speciality], (errProf, resultProf) => {
+        let userQuery = "INSERT INTO users (avatar, full_name, login, email, pass, gender, birth_date, prof_id_fk) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING userid";
+            if (errProf) {
+            console.log(errProf.stack)
+            }
+            //console.log(resultProf)
+            
+        pool.query(userQuery, [data.avatar, data.name, data.user, data.email, data.password, data.gender, data.birthDate, resultProf.rows[0].profid] , (errUser, resultUser) =>{
+            if (errUser) {
+                console.log(errUser.stack)
+            } else {
+                console.log(resultUser.rows[0])
+                res.send(resultUser)
+            }
+        })
     })
-    return res.status;
 });
-
 app.listen(3001, () => {
  console.log('running on port 3001');
 });
